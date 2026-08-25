58792b05b13693059c92e1885f75ad24bb890d1e
bankmotion <harris5555555555555@gmail.com>
2026-08-25 00:20:03 -0700

    chore: remove temporary scripts for LinkedIn integration testing

--- src/scripts/_tmp-vals.ts
+++
@@ -1,5 +0,0 @@
-import { prisma } from '../lib/prisma';
-prisma.scraperSetting
-  .findMany({ where: { key: { contains: 'linkedin' } }, orderBy: { key: 'asc' } })
-  .then((rs) => rs.forEach((r) => console.log(`${r.key} = ${r.value}`)))
-  .finally(() => prisma.$disconnect());
